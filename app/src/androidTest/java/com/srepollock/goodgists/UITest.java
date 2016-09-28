9b7b4c2fb3668d82f7a9b06f99144233953ae358
Spencer Pollock <srepollock@gmail.com>
2016-09-28 00:10:04 -0700

    :beer: Created menu screen. Uses a TableLayout with buttons spanning the entire row. Just this screen works. Going to create checks next to see if the user is logged in or not. This will just be temporary. After that I will create a login screen to then login and view the changed menu.

---
+++ app/src/androidTest/java/com/srepollock/goodgists/UITest.java
@@ -0,0 +1,14 @@
+package com.srepollock.goodgists;
+
+import org.junit.Test;
+
+/**
+ * Created by spencer on 2016-09-27.
+ */
+
+public class UITest {
+    @Test
+    public void menuScreen_SearchButton(){
+        //onView(withId(R.))
+    }
+}
---
+++ app/src/main/java/com/srepollock/goodgists/MenuScreen.java
@@ -0,0 +1,15 @@
+package com.srepollock.goodgists;
+
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MenuScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_screen);
+    }
+}
