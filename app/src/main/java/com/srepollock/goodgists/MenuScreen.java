ea158000996546b156fb63675bb67c622c2b9c7e
Spencer Pollock <srepollock@gmail.com>
2016-09-29 22:50:27 -0700

    :beer: Added search screen. Search gets text update and posts an alert upon entering text and submitting.

--- app/src/main/java/com/srepollock/goodgists/MenuScreen.java
+++ app/src/main/java/com/srepollock/goodgists/MenuScreen.java
@@ -1,9 +1,9 @@
 package com.srepollock.goodgists;
 
-import android.graphics.Typeface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.View;
 
 public class MenuScreen extends AppCompatActivity {
 
@@ -11,5 +11,11 @@ public class MenuScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_screen);
+    }
+
+    public void searchScreen(View view) {
+        // Intent for the activity to open when user selects the notification
+        Intent detailsIntent = new Intent(this, SearchScreen.class);
+        startActivity(detailsIntent);
     }
 }
