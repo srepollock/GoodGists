793760f2740d9182f6f1f8bc84f73d8a1bfb0df5
Spencer Pollock <srepollock@gmail.com>
2016-10-21 02:50:42 -0700

    :sleepy: Going to have to work on this more next. Need to unit test my GitHubController class and fix it so that I can pass it among activities with the information of the user as well as logging in and saving the OAuth key in a file on the device

--- app/src/main/java/com/srepollock/goodgists/GithubController.java
+++ app/src/main/java/com/srepollock/goodgists/GithubController.java
@@ -1,17 +1,105 @@
 package com.srepollock.goodgists;
 
+import android.content.Context;
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import org.kohsuke.github.GHAuthorization;
 import org.kohsuke.github.GitHub;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by Spencer on 2016-10-19.
  */
 
-public class GithubController {
-    private String OAuthKey;
-    public void connect() throws IOException {
+public class GitHubController implements Parcelable {
+    private int mData;
+    private static final ArrayList<String> scope =
+            new ArrayList<>(Arrays.asList(
+                    GHAuthorization.GIST,
+                    GHAuthorization.NOTIFICATIONS,
+                    GHAuthorization.PUBLIC_REPO,
+                    GHAuthorization.USER,
+                    GHAuthorization.USER_FOLLOW
+                    ));
+    private GitHub gitHub;
 
+    // Parcelable (refer http://stackoverflow.com/questions/2139134/how-to-send-an-object-from-one-android-activity-to-another-using-intents)
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel out, int flags) {
+        out.writeInt(mData);
+    }
+
+    // this is used to regenerate your object. All Parcelables must have a CREATOR that implements these two methods
+    public static final Parcelable.Creator<GitHubController> CREATOR =
+            new Parcelable.Creator<GitHubController>() {
+        public GitHubController createFromParcel(Parcel in) {
+            return new GitHubController(in);
+        }
+
+        public GitHubController[] newArray(int size) {
+            return new GitHubController[size];
+        }
+    };
+
+    // example constructor that takes a Parcel and gives you an object populated with it's values
+    private GitHubController(Parcel in) {
+        mData = in.readInt();
+    }
+
+    public GitHubController() {
+
+    }
+
+    public boolean connect(String login, String password) {
+        try {
+            gitHub = GitHub.connectUsingPassword(login, password);
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+            return false;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    public void getOAuthKeyFile(Context appContext, String filename) {
+
+    }
+
+    public void setOAuthKeyFile(Context appContext, String filename) {
+        File f = new File(appContext.getFilesDir(), filename);
+        FileOutputStream fileOutputStream;
+        try {
+            fileOutputStream = appContext.openFileOutput(filename, Context.MODE_PRIVATE);
+//            fileOutputStream.write();
+//            fileOutputStream.close();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+
+    private String createOAuthKey(Context appContext) {
+        String OAuthKey;
+        GHAuthorization ghAuthorization;
+        try {
+            ghAuthorization = gitHub.createToken(scope, appContext.getString(R.string.app_name), null);
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+
+        return null;
     }
 }
 
--- app/src/main/java/com/srepollock/goodgists/LoginScreen.java
+++ app/src/main/java/com/srepollock/goodgists/LoginScreen.java
@@ -9,42 +9,50 @@ import android.widget.EditText;
 
 public class LoginScreen extends AppCompatActivity {
 
+    // TODO Temporary, remove this for future use and use parcelable
+    GitHubController gitHubController = new GitHubController();
+
+    private boolean OAuthResult = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+        // TODO Retrieve parcelable
     }
 
     public void loginToGitHub(View view) {
-        // Get the value of login and check valid username/email
-        EditText usernameLogin = (EditText) findViewById(R.id.usernameLogin);
-        EditText password = (EditText) findViewById(R.id.password);
-        if(checkEmail(usernameLogin.getText().toString())
-                && checkPassword(password.getText().toString())) {
-            // Get
-            finish(); // End activity
+        String username = editTextAsString((EditText) findViewById(R.id.usernameLogin));
+        String password = editTextAsString((EditText) findViewById(R.id.password));
+        if(!checkUser(username) && !checkPassword(password)) {
+            boolean connected = gitHubController.connect(username, password);
+            if (connected) {
+                createOAuthTokenDialog();
+            } else if (!connected){
+                cannotConnectDialog();
+            }
         } else {
             // Display dialog "Invalid username or email"
-            invalidUserEmail();
+            noUserDialog();
         }
     }
 
-    private boolean checkEmail(String u) {
-        if (u != null)
-            return u.contains("@") && u.contains(".");
-        return false;
+    private String editTextAsString(EditText editText) {
+        return editText.getText().toString();
+    }
+
+    private boolean checkUser(String u) {
+        return u.matches("");
     }
 
     private boolean checkPassword(String p) {
-        if (p != null)
-            return true;
-        return false;
+        return p.matches("");
     }
 
-    private void invalidUserEmail() {
+    private void noUserDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         // 2. Chain together various setter methods to set the dialog characteristics
-        builder.setMessage(R.string.invalidUserEmail);
+        builder.setMessage(R.string.noUser);
         builder.setNeutralButton(R.string.close, new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
                 // User cancelled the dialog
@@ -53,5 +61,71 @@ public class LoginScreen extends AppCompatActivity {
         // 3. Get the AlertDialog from create()
         AlertDialog dialog = builder.create();
         dialog.show();
+    }
+
+    private void noPasswordDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        // 2. Chain together various setter methods to set the dialog characteristics
+        builder.setMessage(R.string.noPassword);
+        builder.setNeutralButton(R.string.close, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User cancelled the dialog
+            }
+        });
+        // 3. Get the AlertDialog from create()
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private void didNotCreateOAuthTokenDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        // 2. Chain together various setter methods to set the dialog characteristics
+        builder.setMessage(R.string.didNotCreateOAuthToken);
+        builder.setNeutralButton(R.string.close, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User cancelled the dialog
+            }
+        });
+        // 3. Get the AlertDialog from create()
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private void cannotConnectDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        // 2. Chain together various setter methods to set the dialog characteristics
+        builder.setMessage(R.string.cannotConnect);
+        builder.setNeutralButton(R.string.close, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User cancelled the dialog
+            }
+        });
+        // 3. Get the AlertDialog from create()
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private boolean createOAuthTokenDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        // 2. Chain together various setter methods to set the dialog characteristics
+        DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+          public void onClick(DialogInterface dialog, int which) {
+              switch (which) {
+                  case DialogInterface.BUTTON_POSITIVE:
+                      OAuthResult = true;
+                      break;
+                  case DialogInterface.BUTTON_NEGATIVE:
+                      OAuthResult = false;
+                      break;
+              }
+          }
+        };
+        builder.setMessage(R.string.cannotConnect)
+                .setPositiveButton(R.string.yes, dialogClickListener)
+                .setNegativeButton(R.string.no, dialogClickListener);
+        // 3. Get the AlertDialog from create()
+        AlertDialog dialog = builder.create();
+        dialog.show();
+        return OAuthResult;
     }
 }
